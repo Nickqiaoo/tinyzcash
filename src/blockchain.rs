70b9817ac171832719af14b86e9efe97ab013d71
Nickqiao <nickqiaoo@gmail.com>
2023-05-29 19:56:31 +0800

    feat:add transaction sign

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -1,6 +1,11 @@
-use std::collections::HashMap;
-use crate::{block::Block, iterator::BlockchainIterator, transaction::{Transaction, new_coinbase_tx}, transaction_output::TXOutput};
+use crate::{
+    block::Block,
+    iterator::BlockchainIterator,
+    transaction::{new_coinbase_tx, Transaction},
+    transaction_output::TXOutput,
+};
 use sled;
+use std::{collections::HashMap, error::Error};
 
 const COINBASEDATA: &str = "coinbase";
 
@@ -38,7 +43,7 @@ impl Blockchain {
         for tx in unspent_transactions {
             for out in tx.vout {
                 if out.can_be_unlocked_with(address) {
-                    utxos.push(out.clone());  // Assuming TXOutput implements Clone trait
+                    utxos.push(out.clone()); // Assuming TXOutput implements Clone trait
                 }
             }
         }
@@ -46,10 +51,10 @@ impl Blockchain {
         utxos
     }
 
-    pub fn mine_block(&mut self, transactions :Vec<Transaction>) {
+    pub fn mine_block(&mut self, transactions: Vec<Transaction>) {
         let b = self.db.open_tree("blocksBucket").unwrap();
         let prev_block_hash = b.get(b"l").unwrap().unwrap().to_vec();
-        
+
         let new_block = Block::new(transactions, prev_block_hash);
         self.tip = new_block.hash.to_vec();
 
@@ -57,7 +62,7 @@ impl Blockchain {
         b.insert(b"l", new_block.hash.as_slice()).unwrap();
         b.flush().unwrap();
     }
-    
+
     pub fn iterator(&self) -> BlockchainIterator {
         BlockchainIterator {
             current_hash: self.tip.clone(),
@@ -65,7 +70,11 @@ impl Blockchain {
         }
     }
 
-    pub fn find_spendable_outputs(&self, address: &str, amount: i64) -> (i64, HashMap<String, Vec<i32>>) {
+    pub fn find_spendable_outputs(
+        &self,
+        address: &str,
+        amount: i64,
+    ) -> (i64, HashMap<String, Vec<i32>>) {
         let mut unspent_outputs = HashMap::new();
         let unspent_txs = self.find_unspent_transactions(address);
         let mut accumulated = 0;
@@ -88,9 +97,9 @@ impl Blockchain {
 
         (accumulated, unspent_outputs)
     }
-    
+
     fn find_unspent_transactions(&self, address: &str) -> Vec<Transaction> {
-        let mut unspent_txs :Vec<Transaction>= Vec::new();
+        let mut unspent_txs: Vec<Transaction> = Vec::new();
         let mut spent_txos = HashMap::new();
         let mut bci = self.iterator();
 
@@ -100,7 +109,10 @@ impl Blockchain {
 
                 // 检查所有输出是否被花费
                 for (out_idx, out) in tx.vout.iter().enumerate() {
-                    if spent_txos.get(&tx_id).map_or(false, |v:&Vec<i32>| v.contains(&(out_idx as i32))) {
+                    if spent_txos
+                        .get(&tx_id)
+                        .map_or(false, |v: &Vec<i32>| v.contains(&(out_idx as i32)))
+                    {
                         continue;
                     }
                     if out.can_be_unlocked_with(address) {
@@ -113,7 +125,10 @@ impl Blockchain {
                     for input in &tx.vin {
                         if input.can_unlock_output_with(address) {
                             let in_tx_id = hex::encode(input.txid.clone());
-                            spent_txos.entry(in_tx_id).or_insert_with(Vec::new).push(input.vout);
+                            spent_txos
+                                .entry(in_tx_id)
+                                .or_insert_with(Vec::new)
+                                .push(input.vout);
                         }
                     }
                 }
@@ -124,5 +139,51 @@ impl Blockchain {
         }
 
         unspent_txs
+    }
+
+    fn find_transaction(&self, id: Vec<u8>) -> Result<Transaction, Box<dyn Error>> {
+        let mut bci = self.iterator();
+
+        loop {
+            if let Some(block) = bci.next() {
+                for tx in &block.transactions {
+                    if tx.id == id {
+                        return Ok(tx.clone());
+                    }
+                }
+
+                if block.prev_block_hash.is_empty() {
+                    break;
+                }
+            }
+        }
+
+        Err("TransactionNotFound".into())
+    }
+
+    fn sign_transaction(&self, tx: &mut Transaction, priv_key: SecretKey) {
+        let secp = Secp256k1::new();
+        let mut prev_txs = HashMap::new();
+
+        for vin in tx.vin.iter() {
+            let prev_tx_result = self.find_transaction(vin.txid);
+            if let Ok(prev_tx) = prev_tx_result {
+                prev_txs.insert(hex::encode(prev_tx.id()), prev_tx);
+            }
+        }
+
+        tx.sign(priv_key, prev_txs);
+    }
+    fn verify_transaction(&self, tx: &Transaction) -> bool {
+        let mut prev_txs = HashMap::new();
+
+        for vin in tx.vin.iter() {
+            let prev_tx_result = self.find_transaction(vin.txid);
+            if let Ok(prev_tx) = prev_tx_result {
+                prev_txs.insert(hex::encode(prev_tx.id()), prev_tx);
+            }
+        }
+
+        tx.verify(&prev_txs)
     }
 }
--- src/transaction.rs
+++ src/transaction.rs
@@ -1,8 +1,9 @@
-use serde::{Serialize, Deserialize};
-use sha2::{Sha256, Digest};
-use std::fmt;
+use num::BigUint;
+use serde::{Deserialize, Serialize};
+use sha2::{Digest, Sha256};
+use std::{fmt, collections::HashMap};
 
-use crate::{transaction_output::TXOutput, blockchain::Blockchain, transaction_input::TXInput};
+use crate::{blockchain::Blockchain, transaction_input::TXInput, transaction_output::TXOutput};
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct Transaction {
@@ -28,13 +29,107 @@ impl Transaction {
 
     fn hash(&self) -> Vec<u8> {
         let mut hash = [0; 32];
-    
+
         let mut tx_copy = self.clone();
         tx_copy.id = vec![];
-    
+
         hash = sha256::hash(tx_copy.serialize().as_slice());
-    
+
         hash.to_vec()
+    }
+
+    fn trimmed_copy(&self) -> Transaction {
+        let mut inputs = Vec::new();
+        let mut outputs = Vec::new();
+
+        for vin in &self.vin {
+            inputs.push(TXInput {
+                txid: vin.txid.clone(),
+                vout: vin.vout,
+                signature: None,
+                pub_key: None,
+            });
+        }
+
+        for vout in &self.vout {
+            outputs.push(TXOutput {
+                value: vout.value,
+                pub_key_hash: vout.pub_key_hash.clone(),
+            });
+        }
+
+        Transaction {
+            id: self.id.clone(),
+            vin: inputs,
+            vout: outputs,
+        }
+    }
+
+    pub fn sign(&mut self, private_key: secp256k1::SecretKey, prev_txs: &HashMap<String, Transaction>) {
+        if self.is_coinbase() {
+            return;
+        }
+
+        for vin in &self.vin {
+            if prev_txs
+                .get(&hex::encode(vin.txid.as_slice()))
+                .unwrap()
+                .id
+                .is_none()
+            {
+                panic!("ERROR: Previous transaction is not correct");
+            }
+        }
+        let mut tx_copy = tx.trimmed_copy();
+
+        for (in_id, vin) in tx_copy.vin.iter_mut().enumerate() {
+            let prev_tx = prev_txs.get(&vin.txid).unwrap();
+            vin.signature = None;
+            vin.pub_key = prev_tx.vout[vin.vout].pub_key_hash.clone();
+
+            let tx_copy_message = secp256k1::Message::from_slice(&tx_copy.hash()).unwrap();
+            let context = secp256k1::Secp256k1::new();
+
+            let mut rng = secp256k1::OsRng;
+            let (signature, _) = context.sign(&tx_copy_message, private_key, &mut rng);
+            let (r, s) = signature.serialize();
+
+            let mut signature_vec = Vec::new();
+            signature_vec.extend_from_slice(&r[..]);
+            signature_vec.extend_from_slice(&s[..]);
+
+            self.vin[in_id].signature = Some(signature_vec);
+            tx_copy.vin[in_id].pub_key = None;
+        }
+    }
+
+    pub fn verify(&self, prev_txs: &HashMap<String, Transaction>) -> bool {
+        let mut tx_copy = self.trimmed_copy();
+        let secp = secp256k1::Secp256k1::new();
+        
+        for (in_id, vin) in self.vin.iter().enumerate() {
+            let prev_tx = &prev_txs[&hex::encode(&vin.txid)];
+            tx_copy.vin[in_id].signature = None;
+            tx_copy.vin[in_id].pub_key = prev_tx.vout[vin.vout as usize].pub_key_hash.clone();
+            tx_copy.id = tx_copy.hash();
+            tx_copy.vin[in_id].pub_key = None;
+            
+            let r = BigUint::from_bytes_be(&vin.signature[..(vin.signature.len() / 2)]);
+            let s = BigUint::from_bytes_be(&vin.signature[(vin.signature.len() / 2)..]);
+            let x = BigUint::from_bytes_be(&vin.pub_key[..(vin.pub_key.len() / 2)]);
+            let y = BigUint::from_bytes_be(&vin.pub_key[(vin.pub_key.len() / 2)..]);
+    
+            let pk = PublicKey::from_slice(&secp, &x.to_bytes_be(), &y.to_bytes_be()).unwrap();
+    
+            let sig = Signature::from_der(&secp, &vin.signature).unwrap();
+            //let pubkey = secp256k1::PublicKey::from_slice(&input.pub_key).unwrap();
+            //let signature = secp256k1::Signature::from_der(&input.signature).unwrap();
+            if !secp.verify(&tx_copy.id.into_inner(), &sig, &pk).is_ok() {
+                return false;
+            }
+        }
+    
+        true
     }
 }
 
@@ -44,24 +139,30 @@ impl fmt::Display for Transaction {
     }
 }
 
-
-pub fn new_coinbase_tx(to: &str, data:&str) -> Transaction {
-    let txin = TXInput { 
+pub fn new_coinbase_tx(to: &str, data: &str) -> Transaction {
+    let txin = TXInput {
         txid: vec![],
         vout: -1,
-        script_sig: String::from(data)
+        script_sig: String::from(data),
     };
-    let txout = TXOutput { value: 10, script_pub_key: to.to_string() };
-    let mut tx = Transaction { id: vec![], vin: vec![txin], vout: vec![txout] };
+    let txout = TXOutput {
+        value: 10,
+        script_pub_key: to.to_string(),
+    };
+    let mut tx = Transaction {
+        id: vec![],
+        vin: vec![txin],
+        vout: vec![txout],
+    };
     tx.set_id();
 
     tx
 }
 
-pub fn new_utxo_transaction(from:String, to:String, amount:i64, bc:&Blockchain) -> Transaction{
+pub fn new_utxo_transaction(from: String, to: String, amount: i64, bc: &Blockchain) -> Transaction {
     let mut inputs = Vec::new();
     let mut outputs = Vec::new();
-    
+
     let (acc, valid_outputs) = bc.find_spendable_outputs(from.as_str(), amount);
     if acc < amount {
         panic!("ERROR: Not enough funds");
@@ -69,20 +170,33 @@ pub fn new_utxo_transaction(from:String, to:String, amount:i64, bc:&Blockchain) 
 
     // Build a list of inputs
     for (txid, outs) in valid_outputs {
-
         for out in outs {
-            let input = TXInput { txid: hex::decode(txid.clone()).unwrap(), vout:out, script_sig: from.to_string(), };
+            let input = TXInput {
+                txid: hex::decode(txid.clone()).unwrap(),
+                vout: out,
+                script_sig: from.to_string(),
+            };
             inputs.push(input);
         }
     }
 
     // Build a list of outputs
-    outputs.push(TXOutput { value: amount, script_pub_key: to.to_string() });
+    outputs.push(TXOutput {
+        value: amount,
+        script_pub_key: to.to_string(),
+    });
     if acc > amount {
-        outputs.push(TXOutput { value: acc - amount, script_pub_key: from.to_string() }); // a change
+        outputs.push(TXOutput {
+            value: acc - amount,
+            script_pub_key: from.to_string(),
+        }); // a change
     }
 
-    let mut tx = Transaction { vin: inputs, vout: outputs, id:Vec::new()};
+    let mut tx = Transaction {
+        vin: inputs,
+        vout: outputs,
+        id: Vec::new(),
+    };
     tx.set_id();
 
     tx
